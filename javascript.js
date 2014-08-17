183b8406d0fc34f44c8f215887e39925dbe745e4
janetZhang <liangqin.zhang@foxmail.com>
2014-08-17 14:13:49 -0700

    Javascript +css changed
    Add showAll()  function
    Css expend body background into full screen.
    Define body width as 980px;

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// Show more
+var myObject = {
+	showAll: function(showName,idName){
+		document.getElementById(showName).style.display = "none";
+		document.getElementById(idName).style.display = "inline";
+	}
+
+}
+
+
